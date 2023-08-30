1502cce235e060c81d6393d6e2fb60e405002a9d
Emanuele Concas <conema@outlook.com>
2023-08-30 18:44:44 +0200

    add distribution

--- src/TransLaplaceRand.jl
+++ src/TransLaplaceRand.jl
@@ -4,6 +4,7 @@ using Roots
 import Roots: @set!, bracket, adjust_bracket, NullTracks, isissue, log_message, AbstractUnivariateZeroState
 using InverseLaplace
 using Random
+using Distributions
 
 include("newton_bracketed.jl")
 include("rlaptrans.jl")
